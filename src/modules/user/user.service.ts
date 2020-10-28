d76732f96a021d048a86f455c80b5ac7a49132bb
peter0618 <peterchoi0618@gmail.com>
2020-10-29 01:44:27 +0900

    OneToOne 관계인 Entity CRUD 작성

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,35 +2,63 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from '../profile/profile.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
   ) {}
 
   async add(user: User): Promise<void> {
+    const profile = user.profile;
+    await this.profileRepository.save(profile); // 저장하면 DB에 profile row가 생성되고 생성된 id가 profile 객체의 id에 매핑됩니다.
     await this.userRepository.save(user);
   }
 
-  findAll(): Promise<User[]> {
-    return this.userRepository.find();
+  async findAll(): Promise<User[]> {
+    // return this.userRepository.find({relations: ["profile"]});
+    return this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.profile', 'profile')
+      .orderBy('user.id', 'DESC')
+      .getMany();
   }
 
-  findOne(id: string): Promise<User> {
-    return this.userRepository.findOne(id);
+  async findOne(id: string): Promise<User> {
+    // return this.userRepository.findOne(id);
+    return (
+      this.userRepository
+        .createQueryBuilder('user')
+        .leftJoinAndSelect('user.profile', 'profile')
+        // .whereInIds([id])
+        .where('user.id = :id', { id })
+        .getOne()
+    );
   }
 
-  async modify(user: User): Promise<void> {
-    const userNew = await this.userRepository.findOne(user.id);
-    userNew.firstName = user.firstName;
-    userNew.lastName = user.lastName;
-    userNew.isActive = user.isActive;
-    await this.userRepository.save(userNew);
+  async modify(newUser: User): Promise<void> {
+    // const user = await this.userRepository.findOne(user.id);
+    const user = await this.findOne(newUser.id.toString());
+    user.firstName = newUser.firstName;
+    user.lastName = newUser.lastName;
+    user.isActive = newUser.isActive;
+
+    const profile = user.profile;
+    profile.address = newUser.profile.address;
+    profile.gender = newUser.profile.gender;
+
+    await this.userRepository.save(user);
+    await this.profileRepository.save(profile);
   }
 
   async remove(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    const profile = user.profile;
     await this.userRepository.delete(id);
+    await this.profileRepository.delete(profile.id);
   }
 }
