db7c04416101b0abf2f0b29c68a3d22354ca82cd
peter0618 <peterchoi0618@gmail.com>
2021-04-21 22:39:27 +0900

    user service jest 설정

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,11 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserModule } from './user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Profile } from '../profile/profile.entity';
+import { ProfileModule } from '../profile/profile.module';
 
 describe('UserService', () => {
   let service: UserService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3308,
+          username: 'peter',
+          password: '1234',
+          database: 'typeorm_test_db',
+          entities: ['**/*.entity{.ts,.js}'],
+          // entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }),
+        UserModule,
+        ProfileModule,
+        TypeOrmModule.forFeature([Profile, User]),
+      ],
       providers: [UserService],
     }).compile();
 
@@ -14,5 +35,11 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find all', async () => {
+    const result = await service.findAll();
+    // console.log(result);
+    expect(Array.isArray(result)).toBe(true);
   });
 });
